b7c1eea3147403f5e29bf60bf8f89fa9b07337fd
fedecodelab <federicoguzman.css@gmail.com>
2025-05-18 00:01:29 -0300

    se agrego el hook lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useLenis } from "@/hooks/lenis";
 import { Hero } from "@/components/Hero";
 import { Portfolio } from "@/components/Portfolio";
 import Tecnologies from "@/components/Tecnologies";
@@ -5,6 +8,7 @@ import Timeline from "@/components/timeline/Timeline";
 import { WorkExperience } from "@/components/WorkExperience";
 
 export default function Home() {
+  useLenis();
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-screen bg-gradient-to-b from-green-800 from-0% to-60% to-neutral-900 -z-10 opacity-50" />
