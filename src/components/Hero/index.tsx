3a3c33a829e2a76480c232619e0783aa764d4087
fedecodelab <federicoguzman.css@gmail.com>
2025-06-01 19:55:00 -0300

    Se modifico un transition

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -32,7 +32,7 @@ export const Hero = () => {
             className="block md:hidden"
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 1.5 }}
+            transition={{ duration: 0.5, delay: 0.9 }}
           >
             <TypeAnimation
               cursor={true}
