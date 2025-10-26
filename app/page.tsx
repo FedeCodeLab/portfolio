bec0ca46de728922174d5b5c4cbf212df40802bb
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-25 22:50:48 -0300

    Se elimino el archivo tailwind.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { useRef } from "react";
-import { Hero } from "../components/Hero";
+import { WorkExperience } from "../components/WorkExperience";
 import { Portfolio } from "../components/Portfolio";
 import Tecnologies from "../components/Tecnologies";
 import { Timeline } from "../components/timeline";
-import { WorkExperience } from "../components/WorkExperience";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { Hero } from "../components/Hero";
+import { useRef } from "react";
 
 export default function Home() {
   const experienceRef = useRef(null);
+  const timelineRef = useRef(null);
   const porfolioRef = useRef(null);
   const skillsRef = useRef(null);
-  const timelineRef = useRef(null);
+
   return (
     <div className="bg-[#061932]">
       <Navbar
