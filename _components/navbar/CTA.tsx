f91fbd43372735edd17ddd86ec287408b3bb2498
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-27 16:02:52 -0300

    Modifación de estilos

--- _components/navbar/CTA.tsx
+++ _components/navbar/CTA.tsx
@@ -1,3 +1,4 @@
+import { Download } from "../ui/icons";
 import { ToggleTheme } from "./ToggleTheme";
 import Link from "next/link";
 
@@ -7,10 +8,13 @@ export const CTA = () => {
       <ToggleTheme />
       <Link
         href="/CV_GuzmanFederico_Frontend.pdf"
-        className="font-pixelifySans flex justify-center items-center gap-2 whitespace-nowrap rounded-[50px] cursor-pointer font-[500] px-6 py-2 text-[14px] border-2 border-transparent dark:border-gray-400 bg-gray-200 hover:bg-gray-300 dark:bg-transparent dark:hover:bg-[#1E293B] dark:hover:border-[#1E293B] max-w-[130px] text-neutral-800 dark:text-slate-300 dark:hover:text-white transition duration-300 "
+        className="font-pixelifySans flex justify-center items-center gap-2 whitespace-nowrap rounded-[50px] cursor-pointer font-[500] px-4 py-2 text-[14px] border-2 border-transparent dark:border-gray-400 bg-gray-200 hover:bg-gray-300 dark:bg-transparent dark:hover:bg-[#1E293B] dark:hover:border-[#1E293B] text-neutral-800 dark:text-slate-300 dark:hover:text-white transition duration-300"
         target="_blank"
         download={true}
       >
+        <span>
+          <Download size={14} />
+        </span>
         Descargar CV
       </Link>
     </div>
