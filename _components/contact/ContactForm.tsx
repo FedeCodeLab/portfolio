555596d16ea2002d8db9b9da67d335ee18b4c967
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-29 17:26:45 -0300

    Separación de la parte izquierda del formulario

--- _components/contact/ContactForm.tsx
+++ _components/contact/ContactForm.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
-import { CTA } from "../footer/CTA";
-import { Email, Pin } from "../ui/icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LeftContent } from "./LeftContent";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { z } from "zod";
 
 const schema = z.object({
   name: z.string().min(2, "El nombre es obligatorio"),
@@ -50,47 +49,7 @@ export default function ContactForm() {
 
   return (
     <div className="flex flex-col md:flex-row w-full rounded-[25px]">
-      <div className="flex-1 gap-12 p-10 flex flex-col bg-[#F2EAE2] dark:bg-transparent dark:bg-gradient-to-br dark:from-[#1e293b] dark:to-[#0f172a] rounded-t-[25px] md:rounded-l-[25px] md:rounded-tr-none md:rounded-bl-[25px]">
-        <div className="flex flex-col gap-3">
-          <h3 className="heading-5 !font-[600] text-[#0a0a3a] dark:text-white">
-            ¡Trabajemos juntos!
-          </h3>
-          <p className="max-w-[470px] font-[600] text-[15px]">
-            Estoy abierto a colaborar en proyectos freelance y oportunidades
-            full-time. Si necesitás desarrollo web o querés charlar sobre nuevas
-            ideas, ¡no dudes en contactarme!
-          </p>
-        </div>
-
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center gap-4 bg-[#3098F3] dark:bg-[#0f172a] px-4 py-6 rounded-[12px]">
-            <div className="text-[#3098F3] dark:text-white bg-white dark:bg-blue-800 p-2 rounded-[6px]">
-              <Email size={30} />
-            </div>
-            <div className="text-white">
-              <p>Email</p>
-              <p>federicoguzman.css@gmail.com</p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 bg-[#3098F3] dark:bg-[#0f172a] px-4 py-6 rounded-[12px]">
-            <div className="text-[#3098F3] dark:text-white bg-white dark:bg-blue-800 p-2 rounded-[6px]">
-              <Pin size={30} />
-            </div>
-            <div className="text-white">
-              <p>Ubicación</p>
-              <p>La Rioja, La Rioja, Argentina</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-start gap-3">
-          <p className="font-[600] text-[.9rem]">Sígueme en Redes Sociales</p>
-          <div className="text-white">
-            <CTA />
-          </div>
-        </div>
-      </div>
+      <LeftContent />
 
       <div className="bg-[#f3e3d4] dark:bg-[#0f172a] p-10 flex-1">
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
