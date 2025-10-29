0dbad5c41e7066d1b06d8e4a5307c3cd60a486d0
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-29 18:33:26 -0300

    Se modifico la versión mobile del form

--- _components/contact/LeftContent.tsx
+++ _components/contact/LeftContent.tsx
@@ -3,7 +3,7 @@ import { CTA } from "../footer/CTA";
 
 export const LeftContent = () => {
   return (
-    <div className="flex-1 gap-12 p-10 flex flex-col bg-[#F2EAE2] dark:bg-transparent dark:bg-gradient-to-br dark:from-[#1e293b] dark:to-[#0f172a] rounded-t-[25px] md:rounded-l-[25px] md:rounded-tr-none md:rounded-bl-[25px]">
+    <div className="flex-1 p-[20px] gap-12 md:p-10 flex flex-col bg-[#F2EAE2] dark:bg-transparent dark:bg-gradient-to-br dark:from-[#1e293b] dark:to-[#0f172a]  md:rounded-l-[25px] md:rounded-tr-none md:rounded-bl-[25px]">
       <div className="flex flex-col gap-3">
         <h3 className="heading-5 !font-[600] text-[#0a0a3a] dark:text-white">
           ¡Trabajemos juntos!
@@ -20,9 +20,11 @@ export const LeftContent = () => {
           <div className="text-[#3098F3] dark:text-white bg-white dark:bg-blue-800 p-2 rounded-[6px]">
             <Email size={30} />
           </div>
-          <div className="text-white">
-            <p>Email</p>
-            <p>federicoguzman.css@gmail.com</p>
+          <div className="text-white font-[600]">
+            <p className="text-[15px]">Email</p>
+            <p className="text-[14px] text-neutral-400">
+              federicoguzman.css@gmail.com
+            </p>
           </div>
         </div>
 
@@ -30,9 +32,11 @@ export const LeftContent = () => {
           <div className="text-[#3098F3] dark:text-white bg-white dark:bg-blue-800 p-2 rounded-[6px]">
             <Pin size={30} />
           </div>
-          <div className="text-white">
-            <p>Ubicación</p>
-            <p>La Rioja, La Rioja, Argentina</p>
+          <div className="text-white font-[600]">
+            <p className="text-[15px]">Ubicación</p>
+            <p className="text-[14px] text-neutral-400">
+              La Rioja, La Rioja, Argentina
+            </p>
           </div>
         </div>
       </div>
