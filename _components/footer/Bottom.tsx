92f6a441f9da1dd8d94ee35345ef39950c5308ed
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-30 20:09:29 -0300

    Se modificó el footer

--- _components/footer/Bottom.tsx
+++ _components/footer/Bottom.tsx
@@ -2,8 +2,7 @@ export const Bottom = () => {
   return (
     <div className="text-center lg:text-start flex items-start justify-start">
       <p className="text-[14px]">
-        © 2025 · Federico Pablo Guzmán · Hecho con Next.js · Todos los derechos
-        reservados.
+        © 2025 · Federico Pablo Guzmán · Hecho con Next.js
       </p>
     </div>
   );
